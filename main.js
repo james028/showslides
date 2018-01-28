697f25af8c72cf1558dafa2c41698d51bcdfc5d0
Jakub Kania <kubak.15@gmail.com>
2018-01-28 16:39:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-var slideIndex = 0;
+var slideIndex = 1;
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+  
+showSlides();
 function showSlides() {
     console.log("go");
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+
     slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].classList.add("active");
+
     setTimeout(showSlides, 5000);
 }
-showSlides();
